49e0a1125a6dd67d6f0998d87b5eff2e7f80319c
devv1310 <devanshuvishwakarma58@gmail.com>
2024-07-23 20:02:05 +0530

    python

--- Module/work.py
+++ Module/work.py
@@ -1,5 +1,7 @@
 # syntax from module_name import fun_name,variable_name,class_name
 from module import add as a #as key word is used for making alisting of any class or funtion
+from module import sub as b #as key word is used for making alisting of any class or funtion
 x=int(input("enter your no, "))
 y=int(input("enter your no, "))
 print(a(x,y))
+print(b(x,y))
