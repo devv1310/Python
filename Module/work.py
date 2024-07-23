55ac74d16203134dbc7aa23a46a6d5d3aeb32fc3
devv1310 <devanshuvishwakarma58@gmail.com>
2024-07-23 19:58:38 +0530

    python

--- Module/work.py
+++ Module/work.py
@@ -1,4 +1,3 @@
-
 # syntax from module_name import fun_name,variable_name,class_name
 from module import add as a #as key word is used for making alisting of any class or funtion
 x=int(input("enter your no, "))
