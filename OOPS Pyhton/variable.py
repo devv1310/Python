6e965779dd737522b14ff920fa557d812857f531
devv1310 <devanshuvishwakarma58@gmail.com>
2024-07-30 14:29:04 +0530

    Variable

---
+++ OOPS Pyhton/variable.py
@@ -0,0 +1,45 @@
+#  intance variable jiske value objact badalna ki sath badal jati hai
+# intance variable thorugh const.
+# intance variablr is self.name
+# intance variablr is self.age
+# class Student:
+#     def __init__(self,name,age):
+#         self.name=name  
+#         self.age=age
+#     def display(self):
+#         print("name=",self.name)
+#         print("age=",self.age)
+# obj=Student('dev',25)
+# obj.display()
+# print(obj.name)
+# print(obj.age)
+# obj1=Student('prashu',23)
+# obj1.display()
+# ________________________________________________________________________________________________________
+# intance variable thorugh object
+# class Student:
+#     def __init__(self) :
+#         pass
+#     def display(self):
+#         print("Name=",self.n1)
+#         print("age",self.n2)
+# obj=Student()
+# obj.n1='dev'
+# obj.n2=25
+# obj.display()
+# ________________________________________________________________________________________________________
+# intance variable thorugh Instance 
+# class Student:
+#     def display(self,name,age):
+#         self.n1=name
+#         self.n2=age
+#     def Show(self):
+#         print(self.n1,self.n2)
+# obj=Student()
+# obj.display('dev',25)
+# obj.Show()
+# print(obj.n1,obj.n2)
+# ________________________________________________________________________________________________________
+# Access instance variable :-
+# 1. within the class -> Through Self
+# 2. outside the class -> Through Object
